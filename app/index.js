517f91adba1d4ad84cb231f780926f490f91619f
Kabir Sondhi <ksondhi78@gmail.com>
2016-05-14 00:04:03 -0400

    Created a nested Avatar component consisting of a username, picture and profile link

--- app/index.js
+++ app/index.js
@@ -1,15 +1,57 @@
+var USER_DATA = {
+	name: 'Kabir Sondhi',
+	username: 'ksondhi',
+	image: 'http://ilarge.lisimg.com/image/2994586/1104full-milla-jovovich.jpg'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
+	render: function() {
+		return <img src = {this.props.imageUrl} style = {{height: 100, width: 100}} />
+	}
+
+})
+
+var ProfileLink = React.createClass({
 	render: function() {
 		return (
-			<div> Hello World </div>
-		)
-	}
-});
+			<div>
+				<a href= {"https://www.github.com/" + this.props.username} >
+					{this.props.username}
+				</a>
+			</div>
+			)
 
-ReactDOM.render(
-	<HelloWorld />,
+	}
+})
+
+var ProfileName = React.createClass({
+	render: function() {
+		return (
+			<div> {this.props.name} </div>
+			)
+	}
+
+})
+
+var Avatar = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<ProfilePic imageUrl = {this.props.user.image} />
+				<ProfileName name = {this.props.user.name} />
+				<ProfileLink username = {this.props.user.username} />
+			</div>
+			)
+	}
+
+})
+
+ReactDOM.render( 
+	<Avatar user = {USER_DATA} />,
 	document.getElementById('app')
-);
+)
+
+
