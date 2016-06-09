0b9ad303ff4a3731e5ef140a9319b531e3c1c9e8
Kabir Sondhi <ksondhi78@gmail.com>
2016-06-08 22:41:35 -0400

    More changes related to the react router

--- app/config/routes.js
+++ app/config/routes.js
@@ -15,7 +15,6 @@ var routes = (
             <IndexRoute component={Home}/>
             <Route path='playerOne' header='Player One' component={PromptContainer}/>
             <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer}/>
-            <Route path='battle' component={PromptContainer}/>
         </Route>
     </Router>
 );
